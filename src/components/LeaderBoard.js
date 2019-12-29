3d9a2296cc3986090eebe9165bb100b80e107977
tamasdinh <tamas.dinh@gmail.com>
2019-12-29 18:36:02 +0100

    feat: added LeaderBoard functionality

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import UserCard from './UserCard'
 
 class LeaderBoard extends Component {
+  
   render() {
+    const { userList } = this.props
     return (
-      <div>
-        LeaderBoard
-      </div>
+      <ul>
+        {userList.map(user => (
+          <li key={user.id}>
+            <UserCard
+              user={user}/>
+          </li>
+        ))}
+      </ul>
     )
   }
 }
 
-export default connect()(LeaderBoard)
+function mapStateToProps({ authedUser, users }) {
+  users = Object.values(users)
+  users.map(user => {
+    user.answerScore = Object.keys(user.answers).length
+    user.createdScore = user.questions.length
+  })
+  let userList = []
+  for (let user of users) {
+    userList.push({
+      id: user.id,
+      name: user.name,
+      avatarURL: user.avatarURL,
+      answerScore: user.answerScore,
+      createdScore: user.createdScore,
+      totalScore: user.answerScore + user.createdScore
+    })
+  }
+  userList.sort((a, b) => b.totalScore - a.totalScore)
+
+  return {
+    authedUser,
+    userList
+  }
+}
+
+export default connect(mapStateToProps)(LeaderBoard)
